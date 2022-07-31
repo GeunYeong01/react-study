6d9eabd794854d9947cafcbd770145440b0f978f
geunyeong <rlarmsdud031@naver.com>
2022-07-31 20:12:11 +0900

    chore: 오늘 날짜 알려주는 DateHead 컴포넌트

--- TodoApp/App.js
+++ TodoApp/App.js
@@ -1,13 +1,14 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
 import {SafeAreaView, View, Text, StyleSheet} from 'react-native';
+import DateHead from './DateHead';
 
 function App(){
+  const today = new Date();
+
   return(
     <SafeAreaView>
-      <View>
-        <Text>TodoApp</Text>
-      </View>
+      <DateHead date={today} />
     </SafeAreaView>
   );
 }
