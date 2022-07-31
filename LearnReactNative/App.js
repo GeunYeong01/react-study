e92345de9e302248aa9fb88ee6bca04bddc9695e
geunyeong <rlarmsdud031@naver.com>
2022-07-31 13:04:35 +0900

    chore: 리액트 네이티브 공부

---
+++ LearnReactNative/App.js
@@ -0,0 +1,16 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable eol-last */
+import React from 'react';
+import {SafeAreaView, View, Text} from 'react-native';
+
+const App = () => {
+  return(
+    <SafeAreaView>
+      <View>
+        <Text>Hello React</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
