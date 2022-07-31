11fcf0343a18fd5aad619e6b45450f7f7b860886
geunyeong <rlarmsdud031@naver.com>
2022-07-31 17:24:17 +0900

    chore: TodoApp 생성

---
+++ TodoApp/App.js
@@ -0,0 +1,17 @@
+/* eslint-disable prettier/prettier */
+import React from "react";
+import { SafeAreaView, View, Text, StylesSheet } from "react-native";
+
+function App() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>TodoApp</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StylesSheet.create({});
+
+export default App;
