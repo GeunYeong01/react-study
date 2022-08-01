51e2db9e9f38667b01ae18c6f40dd95722fd76e7
geunyeong <rlarmsdud031@naver.com>
2022-08-01 17:43:49 +0900

    chore: 레이아웃 준비하기

--- TodoApp/App.js
+++ TodoApp/App.js
@@ -1,18 +1,29 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import {SafeAreaView, View, Text, StyleSheet} from 'react-native';
+import {StyleSheet} from 'react-native';
 import DateHead from './DateHead';
+import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
+import AddTodo from './AddTodo';
+import Empty from './Empty';
 
 function App(){
   const today = new Date();
 
   return(
-    <SafeAreaView>
-      <DateHead date={today} />
-    </SafeAreaView>
+    <SafeAreaProvider>
+      <SafeAreaView edges={['bottom']} style={styles.block}>
+        <DateHead date={today} />
+        <Empty />
+        <AddTodo />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  block: {
+    flex: 1,
+  },
+});
 
 export default App;
